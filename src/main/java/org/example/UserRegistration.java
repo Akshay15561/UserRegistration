8951815a1205a890aa7e5dc7f25c82abe38cd762
Akshay15561 <akshaynakawale12@gmail.com>
2024-11-30 13:55:17 +0530

    Welcome to the User Registration System!

---
+++ src/main/java/org/example/UserRegistration.java
@@ -0,0 +1,12 @@
+package org.example;
+
+
+public class UserRegistration {
+    public static void main(String[] args) {
+
+        System.out.printf("Welcome to the User Registration System!");
+
+
+
+    }
+}
