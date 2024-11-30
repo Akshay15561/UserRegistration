cf10c39cf98ec699b0cc467ba2e9e00e8c352841
Akshay15561 <akshaynakawale12@gmail.com>
2024-11-30 14:11:34 +0530

    User need to enter a valid First Name

--- src/main/java/org/example/UserRegistration.java
+++ src/main/java/org/example/UserRegistration.java
@@ -1,12 +1,51 @@
 package org.example;
+import java.util.Scanner;
 
 
 public class UserRegistration {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.printf("Welcome to the User Registration System!");
+        System.out.printf("Welcome to the User Registration System!\n");
 
+        System.out.print("Enter First Name: ");
+        String firstName = scanner.nextLine();
+        if (!isValidFirstName(firstName)) {
+            System.out.println("Invalid First Name\n"+  "It must start with a capital letter and have at least 3 characters");
+            return;
+        }
 
+        System.out.print("Enter Username: ");
+        String username = scanner.nextLine();
 
+        System.out.print("Enter Password: ");
+        String password = scanner.nextLine();
+
+        System.out.print("Enter Email: ");
+        String email = scanner.nextLine();
+
+        System.out.print("Enter Phone Number: ");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.print("Enter Date of Birth (yyyy-mm-dd): ");
+        String dob = scanner.nextLine();
+
+        System.out.println("\nRegistration Details:");
+        System.out.println("First Name: " + firstName);
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("Email: " + email);
+        System.out.println("Phone Number: " + phoneNumber);
+        System.out.println("Date of Birth: " + dob);
+
+        System.out.println("\nRegistration Successful!");
     }
+
+    public static boolean isValidFirstName(String firstName) {
+        return firstName.matches("[A-Z][a-zA-Z]{2,}");
+    }
+
+
+
+
 }
