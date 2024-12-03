c8e3895a3f407f93de29791bce58e0bc59604b5a
Akshay15561 <akshaynakawale12@gmail.com>
2024-12-03 11:08:27 +0530

    JUnit Test to validate the User Entry for First Name, Last Name, Email, Mobile, and Password.

---
+++ src/test/java/org/example/UserRegistrationTest.java
@@ -0,0 +1,64 @@
+package org.example;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class UserRegistrationTest {
+    @Test
+    void testValidFirstName() {
+
+        assertTrue(UserRegistration.isValidName("John"), "Valid first name pass validation.");
+
+
+        assertFalse(UserRegistration.isValidName("jo"), "First name with less than 3 characters fail.");
+        assertFalse(UserRegistration.isValidName("john"), "First name not starting with a capital letter fail.");
+    }
+
+    @Test
+    void testValidLastName() {
+
+        assertTrue(UserRegistration.isValidName("Doe"), "Valid last name pass validation.");
+
+
+        assertFalse(UserRegistration.isValidName("do"), "Last name with less than 3 characters fail.");
+        assertFalse(UserRegistration.isValidName("doe"), "Last name not starting with a capital letter fail.");
+    }
+
+    @Test
+    void testValidEmail() {
+
+        assertTrue(UserRegistration.isValidEmail("test@example.com"), "Valid email pass validation.");
+
+
+        assertFalse(UserRegistration.isValidEmail("testexample.com"), "Email without '@' fail.");
+        assertFalse(UserRegistration.isValidEmail("test@@example.com"), "Email with multiple '@'  fail.");
+        assertFalse(UserRegistration.isValidEmail("test@.com"), "Email with invalid domain fail.");
+        assertFalse(UserRegistration.isValidEmail("test@example..com"), "Email with consecutive dots fail.");
+    }
+    @Test
+    void testValidPhoneNumber() {
+
+        assertTrue(UserRegistration.isValidPhoneNumber("91 9919819801"), "Valid phone numbe pass validation.");
+
+
+        assertFalse(UserRegistration.isValidPhoneNumber("919919819801"), "Phone number without space fail.");
+        assertFalse(UserRegistration.isValidPhoneNumber("91 99198198"), "Phone number with less than 10 digits fail.");
+    }
+
+    @Test
+    void testValidPassword() {
+
+        assertTrue(UserRegistration.isValidPassword("Password@1"), "Valid password pass validation.");
+
+
+        assertFalse(UserRegistration.isValidPassword("pass@1"), "Password with less than 8 characters fail.");
+        assertFalse(UserRegistration.isValidPassword("Password1"), "Password without a special character fail.");
+        assertFalse(UserRegistration.isValidPassword("Password@@1"), "Password with more than one special character fail.");
+        assertFalse(UserRegistration.isValidPassword("password@1"), "Password without an uppercase letter fail.");
+        assertFalse(UserRegistration.isValidPassword("PASSWORD@"), "Password without a digit fail.");
+    }
+
+
+
+
+}
